91e48f546fdc4c3bdd1c07b62188a92f2af71b62
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-05-10 08:24:51 +0700

    add logout icon

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -47,11 +47,8 @@
                     <span class="menu-icon">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
                         <span class="svg-icon svg-icon-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <rect x="2" y="2" width="9" height="9" rx="2" fill="black"></rect>
-                                <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="black"></rect>
-                                <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="black"></rect>
-                                <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="black"></rect>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9da5bc" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                             </svg>
                         </span>
                         <!--end::Svg Icon-->
